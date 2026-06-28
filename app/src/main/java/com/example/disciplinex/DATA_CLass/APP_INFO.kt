809b5fbcb27cba883ca91ef267a77c562f931d62
BishalDas1010 <bishalbishal6659@gmail.com>
2026-06-28 22:41:59 +0530

    app

--- app/src/main/java/com/example/disciplinex/DATA_CLass/APP_INFO.kt
+++ app/src/main/java/com/example/disciplinex/DATA_CLass/APP_INFO.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.graphics.painter.Painter
 data class AppInfo(
     val packageName: String,
     val appName: String,
-    val icon: Painter? // nullable, in case icon can't be loaded
+    val icon: Painter?
 )
